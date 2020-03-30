5298925b376d609184098e3f37ee75f5d4d4a544
Pietrzi <pietrzi@op.pl>
2020-03-30 21:26:57 +0200

    Updates

--- app.js
+++ app.js
@@ -1,152 +1,59 @@
-const nextImageDelay = 2000;
-let currentImage = 0;
-
 // select menu buttons
 
-const firstButton = document.querySelector('#pierwszy');
-const secondButton = document.querySelector('#drugi');
-const thirdButton = document.querySelector('#trzeci');
+const firstButton = document.querySelector('#slider-button');
+const secondButton = document.querySelector('#biography-button');
+const thirdButton = document.querySelector('#gallery-button');
+const filmButton = document.querySelector('#film-button');
 
 // select sections
 
 const firstSection = document.querySelector('#slider-section');
 const secondSection = document.querySelector('#biography-section');
 const thirdSection = document.querySelector('#gallery-section');
+const filmSection = document.querySelector('#film-section');
 
-// select images
-
-const slideshowImages = document.querySelectorAll('.slideshow__img');
 
 // change sections functions
 
-const clickFirst = () => {
+const clickSlider = () => {
     firstSection.classList.add("display");
     secondSection.classList.remove("display");
     thirdSection.classList.remove("display");
-    console.log('click');
+    filmSection.classList.remove("display");
 }
 
-const clickSecond = () => {
+const clickBiography = () => {
     secondSection.classList.add("display");
     firstSection.classList.remove("display");
     thirdSection.classList.remove("display");
-    console.log('click');
+    filmSection.classList.remove("display");
 }
 
-const clickThird = () => {
+const clickGallery = () => {
     thirdSection.classList.add("display");
     firstSection.classList.remove("display");
     secondSection.classList.remove("display");
-    console.log('click');
+    filmSection.classList.remove("display");
+}
+
+const clickFilm = () => {
+    filmSection.classList.add("display");
+    thirdSection.classList.remove("display");
+    firstSection.classList.remove("display");
+    secondSection.classList.remove("display");
 }
 
 // listeners
 
-firstButton.addEventListener('click', clickFirst);
-secondButton.addEventListener('click', clickSecond);
-thirdButton.addEventListener('click', clickThird);
+firstButton.addEventListener('click', clickSlider);
+secondButton.addEventListener('click', clickBiography);
+thirdButton.addEventListener('click', clickGallery);
+filmButton.addEventListener('click', clickFilm);
 
-//console.log(slideshowImages)
-
-
-
+//console.log()
 
 
 
 
 
 
-
-
-
-// const track = document.querySelector('.slideshow');
-// const slides = Array.from(track.children);
-// const prevButton = document.querySelector('.left-button')
-// const nextButton = document.querySelector('.right-button')
-// const firstButton = document.querySelector('#pierwszy');
-// const secondButton = document.querySelector('#drugi');
-// const firstSection = document.querySelector('#raz');
-// const secondSection = document.querySelector('#dwa');
-
-
-// const clickFirst = () => {
-//     firstSection.classList.remove("nodisplay");
-//     firstSection.classList.add("display");
-//     secondSection.classList.remove("display");
-//     secondSection.classList.add("nodisplay");
-//     //autoPlayInterval();
-// }
-
-// const clickSecond = () => {
-//     clearInterval(autoPlayInterval)
-//     secondSection.classList.remove("nodisplay");
-//     secondSection.classList.add("display");
-//     firstSection.classList.remove("display");
-//     firstSection.classList.add("nodisplay");
-// }
-
-
-
-// const slideWidth = slides[0].getBoundingClientRect().width;
-
-// const setSlidesPosition = (slide, i) => slide.style.left = slideWidth * i + 'px';
-
-// slides.forEach(setSlidesPosition);
-
-// const nextAutoImage = () => {
-    
-//     const currentSlide = track.querySelector('.current-slide');
-//     const nextSlide = currentSlide.nextElementSibling;
-
-//     if(nextSlide === null) {
-//         const firstSlide = currentSlide.previousElementSibling.previousElementSibling;
-//         const amountToMove = firstSlide.style.left;
-//         track.style.transform = 'translateX(-' + amountToMove + ')';
-//         currentSlide.classList.remove('current-slide');
-//         firstSlide.classList.add('current-slide');
-//     } else {
-//         const amountToMove = nextSlide.style.left;
-//         track.style.transform = 'translateX(-' + amountToMove + ')';
-//         currentSlide.classList.remove('current-slide');
-//         nextSlide.classList.add('current-slide');
-//     }
-//     console.log('did')
-   
-// }
-
-// const autoPlayInterval = () => setInterval(nextAutoImage, 2000);
-// //const stopInterval = () => clearInterval(autoPlayInterval);
-
-
-
-// const prevSlideMove = () => {
-//     //stopInterval();
-//     const currentSlide = track.querySelector('.current-slide');
-//     const prevSlide = currentSlide.previousElementSibling;
-//     const amountToMove = prevSlide.style.left;
-
-//     track.style.transform = 'translateX(' + amountToMove + ')';
-//     currentSlide.classList.remove('current-slide');
-//     prevSlide.classList.add('current-slide');
-//     console.log('prev')
-// }
-
-// const nextSlideMove = () => {
-//     //stopInterval();
-//     const currentSlide = track.querySelector('.current-slide');
-//     const nextSlide = currentSlide.nextElementSibling;
-//     const amountToMove = nextSlide.style.left;
-
-//     track.style.transform = 'translateX(-' + amountToMove + ')';
-//     currentSlide.classList.remove('current-slide');
-//     nextSlide.classList.add('current-slide');
-//     console.log('next')
-// }
-
-// firstButton.addEventListener('click', clickFirst);
-// secondButton.addEventListener('click', clickSecond);
-// prevButton.addEventListener('click', prevSlideMove);
-// nextButton.addEventListener('click', nextSlideMove);
-
-
-// console.log();
