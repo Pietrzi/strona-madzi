55b37a05b09b6a356a9dd7bdf0f20abb38b18f76
Pietrzi <pietrzi@op.pl>
2020-04-24 15:45:05 +0200

    Updates

--- app.js
+++ app.js
@@ -5,12 +5,116 @@ const biographyButton = document.querySelector('#biography-button');
 const portfolioButton = document.querySelector('#portfolio-button');
 const contactButton = document.querySelector('#contact-button');
 
+const homeSection = document.querySelector('#home-section');
+const demoSection = document.querySelector('#demo-section');
+const gallerySection = document.querySelector('#gallery-section');
+const biographySection = document.querySelector('#biography-section');
+const portfolioSection = document.querySelector('#portfolio-section');
+const contactSection = document.querySelector('#contact-section');
+
+const goToHome = (e) => {
+    homeSection.classList.add('display');
+    demoSection.classList.remove('display');
+    gallerySection.classList.remove('display');
+    biographySection.classList.remove('display');
+    portfolioSection.classList.remove('display');
+    contactSection.classList.remove('display');
+
+    homeButton.classList.add('red-active');
+    demoButton.classList.remove('red-active');
+    galleryButton.classList.remove('red-active');
+    biographyButton.classList.remove('red-active');
+    portfolioButton.classList.remove('red-active');
+    contactButton.classList.remove('red-active');
+}
+
+const goToDemo = (e) => {
+    demoSection.classList.add('display');
+    homeSection.classList.remove('display');
+    gallerySection.classList.remove('display');
+    biographySection.classList.remove('display');
+    portfolioSection.classList.remove('display');
+    contactSection.classList.remove('display');
+
+    demoButton.classList.add('red-active');
+    homeButton.classList.remove('red-active');
+    galleryButton.classList.remove('red-active');
+    biographyButton.classList.remove('red-active');
+    portfolioButton.classList.remove('red-active');
+    contactButton.classList.remove('red-active');
+}
+
+const goToGallery = (e) => {
+    gallerySection.classList.add('display');
+    homeSection.classList.remove('display');
+    demoSection.classList.remove('display');
+    biographySection.classList.remove('display');
+    portfolioSection.classList.remove('display');
+    contactSection.classList.remove('display');
+
+    galleryButton.classList.add('red-active');
+    homeButton.classList.remove('red-active');
+    demoButton.classList.remove('red-active');
+    biographyButton.classList.remove('red-active');
+    portfolioButton.classList.remove('red-active');
+    contactButton.classList.remove('red-active');
+}
+
+const goToBiography = (e) => {
+    biographySection.classList.add('display');
+    homeSection.classList.remove('display');
+    demoSection.classList.remove('display');
+    gallerySection.classList.remove('display');
+    portfolioSection.classList.remove('display');
+    contactSection.classList.remove('display');
+
+    biographyButton.classList.add('red-active');
+    homeButton.classList.remove('red-active');
+    demoButton.classList.remove('red-active');
+    galleryButton.classList.remove('red-active');
+    portfolioButton.classList.remove('red-active');
+    contactButton.classList.remove('red-active');
+}
+
+const goToPortfolio = (e) => {
+    portfolioSection.classList.add('display');
+    homeSection.classList.remove('display');
+    demoSection.classList.remove('display');
+    gallerySection.classList.remove('display');
+    biographySection.classList.remove('display');
+    contactSection.classList.remove('display');
+
+    portfolioButton.classList.add('red-active');
+    homeButton.classList.remove('red-active');
+    demoButton.classList.remove('red-active');
+    galleryButton.classList.remove('red-active');
+    biographyButton.classList.remove('red-active');
+    contactButton.classList.remove('red-active');
+}
+
+const goToContact = (e) => {
+    contactSection.classList.add('display');
+    homeSection.classList.remove('display');
+    demoSection.classList.remove('display');
+    gallerySection.classList.remove('display');
+    biographySection.classList.remove('display');
+    portfolioSection.classList.remove('display');
+
+    contactButton.classList.add('red-active');
+    homeButton.classList.remove('red-active');
+    demoButton.classList.remove('red-active');
+    galleryButton.classList.remove('red-active');
+    biographyButton.classList.remove('red-active');
+    portfolioButton.classList.remove('red-active');
+}
+
+homeButton.addEventListener('click', goToHome);
+demoButton.addEventListener('click', goToDemo);
+galleryButton.addEventListener('click', goToGallery);
+biographyButton.addEventListener('click', goToBiography);
+portfolioButton.addEventListener('click', goToPortfolio);
+contactButton.addEventListener('click', goToContact);
 
 
-//console.log()
-
-
-
-
-
-
+// console.log('click')
+// console.log(e.target)
