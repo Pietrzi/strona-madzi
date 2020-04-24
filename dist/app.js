b78687e821e15931cbd45613c8dd5eedf486dae6
Pietrzi <pietrzi@op.pl>
2020-04-24 12:34:04 +0200

    navi comming

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,11 @@
+const homeButton = document.querySelector('#home-button');
+const demoButton = document.querySelector('#demo-button');
+const galleryButton = document.querySelector('#gallery-button');
+const biographyButton = document.querySelector('#biography-button');
+const portfolioButton = document.querySelector('#portfolio-button');
+const contactButton = document.querySelector('#contact-button');
+
+
 
 //console.log()
 
