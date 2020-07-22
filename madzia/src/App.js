21ca395af1700a076db68a24ac29866130124f63
Pietrzi <pietrzi@op.pl>
2020-07-22 21:08:57 +0200

    Routing almost done

--- madzia/src/App.js
+++ madzia/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Demo from './components/Demo';
@@ -10,15 +13,19 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-      <Demo />
-      <Gallery />
-      <Biography />
-      <Portfolio />
-      <Contact />
-    </>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/demo' component={Demo} />
+        <Home />
+        <Demo />
+        <Gallery />
+        <Biography />
+        <Portfolio />
+        <Contact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
